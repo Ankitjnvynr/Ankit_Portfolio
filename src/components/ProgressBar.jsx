6fbe5e5fae54f1d7e0766537c70d41dab55f6db0
unknown <ankitbkana@outlook.com>
2024-01-22 23:41:21 +0530

    skill section added

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -6,13 +6,13 @@ function ProgressBar({ tooltip }) {
       <div className=" relative h-1 rounded-md   flex border">
         <div className="progress w-[15%]  h-1 rounded-md bg-blue-700"></div>
         {tooltip ? (
-          <span class="relative flex h-3 w-3 -translate-x-1 -translate-y-1">
-            <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-blue-500 opacity-75"></span>
-            <span class="relative inline-flex rounded-full h-3 w-3 bg-blue-700 "></span>
-            <span class="absolute translate-y-5 -translate-x-8 inline-flex rounded border border-blue-700 px-3 py-1 font-bold text-blue-700 ">
+          <span className="relative flex h-3 w-3 -translate-x-1 -translate-y-1">
+            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-blue-500 opacity-75"></span>
+            <span className="relative inline-flex rounded-full h-3 w-3 bg-blue-700 "></span>
+            <span className="absolute translate-y-5 -translate-x-8 inline-flex rounded border border-blue-700 px-3 py-1 font-bold text-blue-700 ">
               BigByte
             </span>
-            <span class="absolute translate-y-[14px] bg-white   inline-flex  border-l rotate-45  border-t border-blue-700 h-3 w-3 "></span>
+            <span className="absolute translate-y-[14px] bg-white   inline-flex  border-l rotate-45  border-t border-blue-700 h-3 w-3 "></span>
           </span>
         ) : (
           ""
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -15,100 +15,100 @@ import {
 
 function Skills() {
   return (
-    <div className="mt-2 mb-5 md:mb-20">
+    <div className="mt-2 mb-10 md:mb-20">
       <h1 className="text-center text-3xl font-bold mt-6 mb-2 text-blue-700 ">
         Skills
       </h1>
-      <div className=" grid grid-cols-1 gap-4 md:gap-5 md:grid-cols-4">
+      <div className=" grid grid-cols-1 gap-4 lg:grid-cols-3 md:gap-5 md:grid-cols-2 xl:grid-cols-4">
         <div className="flex items-center gap-2 text-2xl font-bold w-full ">
           <SiAdobephotoshop className=" text-cyan-700 text-5xl" />{" "}
-          <p className="w-full my-3">
+          <div className="w-full my-3"> 
             Adobe Photoshop
             <div className=" relative h-1 rounded-md w-full   flex border">
               <div className=" w-[85%]  h-1 rounded-md bg-blue-700"></div>
             </div>
-          </p>
+          </div>
         </div>
         <div className="flex items-center gap-2 text-2xl font-bold">
           <SiAdobepremierepro className=" text-cyan-700 text-5xl" />{" "}
-          <p className="w-full">
+          <div className="w-full my-3">
             Adobe Premiere Pro
             <div className=" relative h-1 rounded-md   flex border">
               <div className=" w-[55%]  h-1 rounded-md bg-blue-700"></div>
             </div>
-          </p>
+          </div>
         </div>
         <div className="flex items-center gap-2 text-2xl font-bold">
           <SiAdobeaftereffects className=" text-cyan-700 text-5xl" />{" "}
-          <p className="w-full">
+          <div className="w-full my-3">
             Adobe After Effects
             <div className=" relative h-1 rounded-md   flex border">
               <div className=" w-[65%]  h-1 rounded-md bg-blue-700"></div>
             </div>
-          </p>
+          </div>
         </div>
         <div className="flex items-center gap-2 text-2xl font-bold">
           <SiCanva className=" text-cyan-700 text-5xl" />{" "}
-          <p className="w-full">
+          <div className="w-full my-3">
             Canva
             <div className=" relative h-1 rounded-md   flex border">
               <div className=" w-[85%]  h-1 rounded-md bg-blue-700"></div>
             </div>
-          </p>
+          </div>
         </div>
         <div className="flex items-center gap-2 text-2xl font-bold">
           <SiHtml5 className=" text-cyan-700 text-5xl" />{" "}
-          <p className="w-full">
+          <div className="w-full my-3">
             HTML
             <div className=" relative h-1 rounded-md   flex border">
               <div className=" w-[90%]  h-1 rounded-md bg-blue-700"></div>
             </div>
-          </p>
+          </div>
         </div>
         <div className="flex items-center gap-2 text-2xl font-bold">
           <SiCss3 className=" text-cyan-700 text-5xl" />{" "}
-          <p className="w-full">
+          <div className="w-full my-3">
             CSS
             <div className=" relative h-1 rounded-md   flex border">
               <div className=" w-[80%]  h-1 rounded-md bg-blue-700"></div>
             </div>
-          </p>
+          </div>
         </div>
         <div className="flex items-center gap-2 text-2xl font-bold">
           <SiJavascript className=" text-cyan-700 text-5xl" />{" "}
-          <p className="w-full">
+          <div className="w-full my-3">
             JavaScript
             <div className=" relative h-1 rounded-md   flex border">
               <div className=" w-[65%]  h-1 rounded-md bg-blue-700"></div>
             </div>
-          </p>
+          </div>
         </div>
         <div className="flex items-center gap-2 text-2xl font-bold">
           <SiMeta className=" text-cyan-700 text-5xl" />{" "}
-          <p className="w-full">
+          <div className="w-full my-3">
             Facebook Meta Ads
             <div className=" relative h-1 rounded-md   flex border">
               <div className=" w-[50%]  h-1 rounded-md bg-blue-700"></div>
             </div>
-          </p>
+          </div>
         </div>
         <div className="flex items-center gap-2 text-2xl font-bold">
           <SiPhp className=" text-cyan-700 text-5xl" />{" "}
-          <p className="w-full">
+          <div className="w-full my-3">
             Php
             <div className=" relative h-1 rounded-md   flex border">
               <div className=" w-[75%]  h-1 rounded-md bg-blue-700"></div>
             </div>
-          </p>
+          </div>
         </div>
         <div className="flex items-center gap-2 text-2xl font-bold">
           <SiReact className=" text-cyan-700 text-5xl" />{" "}
-          <p className="w-full">
+          <div className="w-full my-3">
             React
             <div className=" relative h-1 rounded-md   flex border">
               <div className=" w-[40%]  h-1 rounded-md bg-blue-700"></div>
             </div>
-          </p>
+          </div>
         </div>
       </div>
     </div>
