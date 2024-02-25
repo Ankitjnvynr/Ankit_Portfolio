e7e4b51e7a7035f6dbe21e575f909e16f3dc5144
unknown <ankitbkana@outlook.com>
2024-02-25 09:34:23 +0530

    some changes apply related to social links

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import MyWork from './components/MyWork'
 import Experience from './components/Experience'
 import Skills from './components/Skills'
 import Education from './components/Education'
+import Social from './components/Social'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,8 +15,9 @@ function App() {
 
   return (
     <>
-      <div className='max-w-[1300px] w-[95%] m-auto bg-white my-4 rounded-xl overflow-hidden '>
+      <div className='max-w-[1300px] w-[95%] m-auto bg-white shadow-lg my-4 rounded-xl overflow-hidden '>
         <Navbar />
+        <Social />
         <HeroSection />
         <AboutSection/>
         <Experience />
