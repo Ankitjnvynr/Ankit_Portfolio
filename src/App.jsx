9523592020edaa01bde1a1da6de514d05b991063
unknown <ankitbkana@outlook.com>
2024-06-13 21:33:56 +0530

    trying browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
+
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -9,22 +10,32 @@ import GrapicDesigns from './GrapicDesigns';
 import MotionGraphics from './MotionGraphics';
 
 function App() {
-  const [count, setCount] = useState(0);
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />,
+    },
+    {
+      path: '/about',
+      element: <About />,
+    },
+    {
+      path: '/graphic-designs',
+      element: <GrapicDesigns />,
+    },
+    {
+      path: '/motion-graphics',
+      element: <MotionGraphics />,
+    },
+  ]);
+  
 
   return (
     <>
       <div className="max-w-[1300px] w-[95%] m-auto bg-white shadow-lg my-4 rounded-xl overflow-hidden ">
         <Navbar />
-        <Router>
-          <div>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/graphic-designs" element={<GrapicDesigns />} />
-              <Route path="/motion-graphics" element={<MotionGraphics />} />
-            </Routes>
-          </div>
-        </Router>
+        <RouterProvider router={router}  />
       </div>
       <div className="max-w-[1300px] w-[95%] m-auto text-white  my-4 rounded-xl overflow-hidden ">
         <Footer />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   // const _ = require('lodash');
 
   const menus = {
     "Home": "/",
-    "About": "#",
+    "About": "/about",
     "Hire Me": "#",
     "My work": "#",
   }
@@ -20,7 +21,9 @@ function Navbar() {
             Object.entries(menus).map(menu => {
               let link = menu[1];
               let name = menu[0];
-              return <li key={name} className=' cursor-pointer hover:text-black hover:bg-blue-100  border-b-2  rounded hover:-translate-y-[2px]  transition-all'><a className='cursor-pointer p-1 md:py-2 md:px-4' href={link}>{name}</a></li>
+              return <li key={name} className=' cursor-pointer hover:text-black hover:bg-blue-100  border-b-2  rounded hover:-translate-y-[2px]  transition-all'>
+                <a className='cursor-pointer p-1 md:py-2 md:px-4' href={link}>{name}</a>
+              </li>
               
               
             })
