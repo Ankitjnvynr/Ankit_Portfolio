281dc9bc9ea492ca928767a48224645af3c33d1a
unknown <ankitbkana@outlook.com>
2024-06-19 21:15:47 +0530

    tried routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-
-import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import './App.css';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+
 import Home from './Home';
 import About from './components/About';
-import GrapicDesigns from './GrapicDesigns';
-import MotionGraphics from './MotionGraphics';
+import GraphicDesigns from './GrapicDesigns'
+import MotionGraphics from './MotionGraphics'
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 
-function App =() => {
+function App () {
   const router = createBrowserRouter([
     {
       path: '/',
@@ -22,22 +23,21 @@ function App =() => {
     },
     {
       path: '/graphic-designs',
-      element: <GrapicDesigns />,
+      element: <GraphicDesigns />,
     },
     {
       path: '/motion-graphics',
       element: <MotionGraphics />,
     },
   ]);
-  
 
   return (
     <>
-      <div className="max-w-[1300px] w-[95%] m-auto bg-white shadow-lg my-4 rounded-xl overflow-hidden ">
+      <div className="max-w-[1300px] w-[95%] m-auto bg-white shadow-lg my-4 rounded-xl overflow-hidden">
         <Navbar />
-        <RouterProvider router={router}  />
+        <RouterProvider router={router} />
       </div>
-      <div className="max-w-[1300px] w-[95%] m-auto text-white  my-4 rounded-xl overflow-hidden ">
+      <div className="max-w-[1300px] w-[95%] m-auto text-white my-4 rounded-xl overflow-hidden">
         <Footer />
       </div>
     </>
